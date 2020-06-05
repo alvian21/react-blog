e2dfd6512cc247b502ab4b84ee4c0c2f5df759c0
Alvian <alvian.ardhiansyah@gmail.com>
2020-06-05 23:26:34 +0700

    revisi

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user')
 mongoose.connect('mongodb+srv://blog:Hello321@cluster0-xckvl.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
     { useNewUrlParser: true }).then(() => console.log('DB connected'))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userdata) => {
+        if (err) return res.json({ success: false, err })
+    })
+    return res.status(200).json({success:true})
 })
-
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
